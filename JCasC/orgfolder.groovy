a253278e7570a6668989612ecf64a5f3ab9b329a
mwdle <mwdle@mwdle.com>
2025-08-18 22:53:22 -0600

    Initial Commit

---
+++ JCasC/orgfolder.groovy
@@ -0,0 +1,33 @@
+import jenkins.model.Jenkins
+
+organizationFolder(System.getenv('ORGFOLDER_NAME')) {
+    displayName(System.getenv('ORGFOLDER_DISPLAY_NAME'))
+    description(System.getenv('ORGFOLDER_DESCRIPTION'))
+    triggers {
+        periodicFolderTrigger {
+            interval('24h')
+        }
+    }
+    organizations {
+        gitea { // Token requires repository:read/write, user:read, and organization:read permissions
+            serverUrl(System.getenv('GIT_SERVER_URL'))
+            repoOwner(System.getenv('GIT_USERNAME'))
+            credentialsId('git-creds')
+            traits {
+                giteaExcludeArchivedRepositories {}
+                giteaBranchDiscovery {
+                    strategyId(1) // Exclude branches that are also filed as PRs
+                }
+                giteaWebhookRegistration {
+                    mode('ITEM')
+                }
+            }
+        }
+    }
+    orphanedItemStrategy {
+        discardOldItems {
+            daysToKeep(730)
+            numToKeep(10000)
+        }
+    }
+}
