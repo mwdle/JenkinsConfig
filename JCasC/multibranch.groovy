333ef638240c1a6bd0c690a3813e68a18c1ae2a4
mwdle <mwdle@mwdle.com>
2025-09-07 20:20:11 -0600

    Removed incorrectly placed traits block

--- JCasC/multibranch.groovy
+++ JCasC/multibranch.groovy
@@ -5,10 +5,6 @@ multibranchPipelineJob('DinoGlue') {
         git {
             remote("${System.getenv('GIT_SERVER_URL')}/Dino3Harris/DinoGlue.git")
             credentialsId('git-creds')
-            traits {
-                gitTagDiscovery()
-                gitBranchDiscovery()
-            }
         }
     }
     orphanedItemStrategy {
